070f7a917f379e448334d5b466f7f8087984bf65
impanyu <impanyu@gmail.com>
2024-09-09 16:21:18 -0500

    test

--- experiments.py
+++ experiments.py
@@ -175,7 +175,7 @@ def main():
         # generate populating query and query 
         db_populating_query,user_query,data = read_data(dataset,"sql",scale, balance, overlap, operation)
 
-        user_query = "SELECT Distinct Language FROM `countrylanguage` WHERE `Percentage` BETWEEN 20.0 AND 40.0;"
+        #user_query = "SELECT Distinct Language FROM `countrylanguage` WHERE `Percentage` BETWEEN 20.0 AND 40.0;"
 
 
         drop_db_query = data["drop_database"][0]
