f1ea890a3fd8675b58932f32d71befc8a77bc2a0
impanyu <impanyu@gmail.com>
2024-09-09 22:33:44 -0500

    test

--- experiments.py
+++ experiments.py
@@ -284,12 +284,20 @@ def main():
 
                 result = json.loads(result)
                 if not type(result) == list:
+                    print(true_result)
+                    print(result)
+
+                    print(f"Accuracy: {accuracy}")
                     continue
                 
                 if len(result) > 0:
                     if type(result[0]) == list:
                         result = [r[0] for r in result]
                     else:
+                        print(true_result)
+                        print(result)
+
+                        print(f"Accuracy: {accuracy}")
                         continue
             
 
