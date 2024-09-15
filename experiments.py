fe092190ffb08d4e9544ffaed9b649add35ba8a3
impanyu <impanyu@gmail.com>
2024-09-15 12:38:37 -0500

    test

--- experiments.py
+++ experiments.py
@@ -230,6 +230,8 @@ def generate_query_result_pair(common_prompts,all_prompts,encoding,scale, balanc
 
 
 def calculate_accuracy(true_result, result,user_prompt):
+    # result is a string
+    # true_result is an array
     if "Fail" in true_result:
         if "Fail" in result:
             return 1
