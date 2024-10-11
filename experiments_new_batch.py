1204f4e8ec08e49c168d5459b29059c60f2195cf
impanyu <impanyu@gmail.com>
2024-10-11 15:50:27 -0500

    test

--- experiments_new_batch.py
+++ experiments_new_batch.py
@@ -622,7 +622,7 @@ def main():
 
 
     #current_model = model
-    if model in ["llama3.1-8B","mistral","gemma2","codellama","phi3"]:
+    if model in ["gpt4","llama3.1-8B","mistral","gemma2","codellama","phi3"]:
         all_model = [model]
     else:
         #all_model = ["llama3.1-8B","mistral","gemma2","codellama","phi3"]
