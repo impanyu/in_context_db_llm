e52a323f664a155cecbdcc4c1e589a7cde7e7662
impanyu <impanyu@gmail.com>
2024-10-12 17:15:32 -0500

    test

--- experiments_new_batch.py
+++ experiments_new_batch.py
@@ -689,7 +689,7 @@ def main():
                                     accuracy = run_experiment(common_prompts,all_prompts,current_encoding,current_scale, current_balance, current_overlap, current_model, current_prompting, current_operation)
                                     output[f"{current_model}_{current_prompting}_{current_encoding}_{current_operation}_{current_scale}_{current_balance}_{current_overlap*2}"] = accuracy
 
-                                    with open(f"output_{model}.json", "w") as file:
+                                    with open(f"output_{current_model}.json", "w") as file:
                                         json.dump(output, file)
                                         file.flush()
                                 else:
