33487650a0c5223ba9c68d194bc50e26c49bf77a
impanyu <impanyu@gmail.com>
2024-10-15 14:00:28 -0500

    test

--- experiments_new_batch_cp.py
+++ experiments_new_batch_cp.py
@@ -520,7 +520,7 @@ def run_experiment(common_prompts,all_prompts,encoding,scale, balance, overlap, 
                 
             except APITimeoutError:
                 t = t-1
-                print("The request timed out.")
+                print("The request timed out.",flush=True)
                 time.sleep(1)
                 continue
             #print("end api call")
