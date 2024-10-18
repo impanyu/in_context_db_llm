4aed5764ccbc1b5841032b6540bcd2d2be84348f
impanyu <impanyu@gmail.com>
2024-10-18 14:41:12 -0500

    test

--- batch_experiments.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 experiments_new_batch.py --model all --prompting few_shot --encoding sql --operation single_filtering --scale -1 
-python3 experiments_new_batch.py --model all --prompting few_shot --encoding sql --operation single_filtering --balance -1
-python3 experiments_new_batch.py --model all --prompting few_shot --encoding sql --operation single_filtering --overlap -1
