09d6b757c94464c2720cedd6758c5e335604460c
impanyu <impanyu@gmail.com>
2024-10-17 20:10:24 -0500

    test

--- experiments_new_batch.py
+++ experiments_new_batch.py
@@ -791,7 +791,7 @@ def main():
                 if operation in ["select","update","delete","insert","no_filtering","single_filtering","double_filtering","triple_filtering","range_filtering","ranking","count","single_table","double_table","three_table"]:  
                     all_operation = [operation]
                 else:
-                    all_operation = ["update","no_filtering","single_table"]#["update","delete","insert","no_filtering","single_filtering","double_filtering","triple_filtering","range_filtering","ranking","count","single_table","double_table","three_table"]
+                    all_operation =["update","delete","insert","no_filtering","single_filtering","double_filtering","triple_filtering","range_filtering","ranking","count","single_table","double_table","three_table"]
                 for current_operation in all_operation:
                     if scale <0:
                         all_scale = [10,50,100,150,200,250,300,350,400]
